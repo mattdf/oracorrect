cb6b33c12e9d614eca1dbe7ec0cbf449fe34053e
plutoegg <will.harborne@gmail.com>
2016-09-11 11:42:32 +0100

    'debugged' using ethereum studio

--- OraclizeOracle.sol
+++ OraclizeOracle.sol
@@ -6,20 +6,20 @@ contract OraclizeOracle  is usingOraclize{
 	mapping(bytes32 => uint) oraclizeIdToId;
 	OracleReceiver oracleReceiver;
 
-	public function OraclizeOracle(OracleReceiver oracleReceiver){
+	function OraclizeOracle(OracleReceiver oracleReceiver){
 		oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
 	}
 
-	public function query(uint id, string queryString){
+	function query(uint id, string queryString){
 
 		bytes32 oraclizeId = oraclize_query("URL", queryString); //TODO pass fee (msg.value) ?
 		oraclizeIdToId[oraclizeId] = id;
 	}
 
-	public function __callback(bytes32 myid, string result, bytes proof){
+	function __callback(bytes32 myid, string result, bytes proof){
 		//TODO check the proof
 		uint id = oraclizeIdToId[myid];
 		//TODO get value out of string
-		oracleReceiver.onData(id,value);
+		oracleReceiver.onData(id,parseInt(result));
 	}
 }
